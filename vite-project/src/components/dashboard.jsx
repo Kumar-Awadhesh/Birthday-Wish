11af0ebcf6143a4f2511d471f242f6ca033a134c
Awadhesh Kumar <kumaravi0506@gmail.com>
2026-09-01 02:26:36 +0530

    search box input verification added

--- vite-project/src/components/dashboard.jsx
+++ vite-project/src/components/dashboard.jsx
@@ -7,15 +7,36 @@ const Dashboard = () => {
     const {user, setUser} = useContext(BirthdayContext);
     const navigate = useNavigate();
 
-
+    let count = 0;
     const userAuthentication = () => {
-        if(user.toLocaleLowerCase() === userName.toLocaleLowerCase()){
-            alert("It is you my Lady!!");
-            navigate("/waiting")
+        if(userName !== ""){
+            if(user.toLocaleLowerCase() === userName.toLocaleLowerCase()){
+                alert(
+                    "It's you my Elaichi!!",
+                    "Let me take you to your Surprise!"
+                );
+                navigate("/waiting")
+            }
+            else if(userName.toLocaleLowerCase() === "titli" || userName.toLocaleLowerCase() === "biscuit" || userName.toLocaleLowerCase() === "sunshine"){
+                alert("not this one! more and very special name of yours!")
+            }
+            else if(count === 0){
+                count++;
+                alert("Enter your special name!")
+            }
+            else if(count === 1){
+                count++;
+                alert("Enter the special name, I gave you!")
+            }
+            else{
+                count = 0;
+                alert("hmm...! lets take a moment and think more deep and try again!")
+            }
         }
         else{
-            alert("Enter your special name!")
+            alert("Enter your special name to begin with!")
         }
+
     }
 
    
@@ -23,7 +44,7 @@ const Dashboard = () => {
         <>
             <main className="container">
                 <div className="user-input-container">
-                    <input type="text" placeholder="Enter your name precious one!" onChange={(e)=>setUserName(e.target.value)} />
+                    <input type="password" placeholder="Enter your name, precious one!" onChange={(e)=>setUserName(e.target.value)} />
                     <button onClick={userAuthentication}>It's Me</button>
                 </div>
             </main>
