4f4e55873c4cfeca91e6c529a7db9f1110f0de9b
Awadhesh Kumar <kumaravi0506@gmail.com>
2026-09-07 01:52:18 +0530

    added background image to openWhen component

--- vite-project/src/components/openWhen.jsx
+++ vite-project/src/components/openWhen.jsx
@@ -7,9 +7,10 @@ const OpenWhen = () => {
 
     return(
         <>
-            <main className="open-when-container">
+            <main className="open-when-container smooth-navigation">
                <iframe src="" frameborder="0"></iframe> 
                <h2>More Comming Soon... <br /> Keep in Touch!</h2>
+               <a href="" target="blank">Go</a>
             </main>
         </>
     )
