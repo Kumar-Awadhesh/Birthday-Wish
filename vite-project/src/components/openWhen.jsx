6496ef676b88fbb87a08ecaed92d4083e3537553
Awadhesh Kumar <kumaravi0506@gmail.com>
2026-09-26 01:46:36 +0530

    made some adjustment to open when component

--- vite-project/src/components/openWhen.jsx
+++ vite-project/src/components/openWhen.jsx
@@ -17,6 +17,7 @@ const OpenWhen = () => {
             try {
                 const song = await axios.get("/songs.json")
                 setSongs(song.data)
+                console.log(song.data.openWhen.mycollection)
             } 
             catch (error) {
                 console.log(error)
@@ -33,31 +34,37 @@ const OpenWhen = () => {
                {
                 inspiration &&
 
-                    <div>
-                        <div className="open-when-overlay"></div>
-                        <iframe src="https://audio.com/image-2/audio/selena-gomez-who-says-lyrics" frameborder="1" scrolling="no"  ></iframe>
-                    </div> 
+                    songs?.openWhen?.inspiration?.map((song, index)=> (
+                        <div key={song.id}>
+                            <div className="open-when-overlay"></div>
+                            <iframe src={song.song} frameBorder="0" scrolling="no"></iframe>
+                        </div>
+                    ))
                 }
                 {
                     moodDown &&
-                    <div>
-                        <div className="open-when-overlay"></div>
-                        <iframe src="https://audio.com/stacy-powell/audio/katy-perry-teenage-dream-lyrics" frameborder="0" scrolling="no" ></iframe>
-                    </div>
+                    songs?.openWhen?.moodDown?.map((song, index)=> (
+                        <div key={song.id}>
+                            <div className="open-when-overlay"></div>
+                            <iframe src={song.song} frameBorder="0" scrolling="no"></iframe>
+                        </div>
+                    ))
                 }
                 {
                     lonely &&
-                    <div>
-                        <div className="open-when-overlay"></div>
-                        <iframe src="https://audio.com/anna-1867369262008560/audio/selena-gomez-ft-marshmello-wolves" frameborder="0" scrolling="no" ></iframe>
-                    </div>
+                    songs?.openWhen?.lonely?.map((song, index)=> (
+                        <div key={song.id}>
+                            <div className="open-when-overlay"></div>
+                            <iframe src={song.song} frameBorder="0" scrolling="no"></iframe>
+                        </div>
+                    ))
                 }
                 {
                     myCollection &&
-                    songs.openWhen.mycollection.map((song, index)=> (
+                    songs?.openWhen?.mycollection?.map((song, index)=> (
                         <div key={song.id}>
                             <div className="open-when-overlay"></div>
-                            <iframe src={song.song} frameborder="0" scrolling="no"></iframe>
+                            <iframe src={song.song} frameBorder="0" scrolling="no"></iframe>
                         </div>
                     ))
                 }
